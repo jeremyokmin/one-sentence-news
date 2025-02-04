1cafd3f12b8ab0ba027ed3912019198fa89c386c
Jeremy Okmin <jeremyokmin@Jeremys-MacBook-Air-3.local>
2025-02-04 10:42:38 -0800

    clean up

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { io } from 'socket.io-client';
-import { NewsManager } from './NewsManager.js';
-
-const socket = io();
-const newsManager = new NewsManager();
-
-socket.on('newPost', (post) => {
-    newsManager.addEntryToDisplay(post);
-});
